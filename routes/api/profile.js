dfe999844a77a710d7520a4124d0b1f957eb8d64
Prabu Thirumoorthi <pthirumoorthi@altimetrik.com>
2019-12-30 03:29:24 +0530

    Front end react js changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request=require('request');
+const config=require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
@@ -269,5 +271,31 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
         console.error(err.message);
         return res.status(500).send('Server error!');
     }
+});
+//@route GET api/profile/github/:username
+//@desc  GET user repos from git hub
+//@access Public
+router.get('/github/:username',(req,res)=>{
+try {
+    const options={
+        uri:`https://api.github.com/users/${
+            req.params.username
+        }/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}
+        &client_secret=${config.get('githubSecretId')}`,
+        method:'GET',
+        headers:{'user-agent':'nodejs'}
+    };
+    request(options,(error,response,body)=>{
+        if(error)console.error(error);
+        
+        if(response.statusCode!==200){
+          return  res.status(404).json({msg:'No github profile found'});
+        }
+        res.json(JSON.parse(body));
+    });
+} catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server error!');
+}
 });
 module.exports = router;
